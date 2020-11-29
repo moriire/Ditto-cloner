681b374263264654fb9d58ecd8f19a105d81ab1a
Mobolaji Abdulsalam <56216197+moriire@users.noreply.github.com>
2020-11-28 23:28:38 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
+from setuptools import setup, find_packages 
 
-setup(
-    name='gecko.py',
-    version='0.1',
-    py_modules=['gecko.py'],
-    install_requires=[
-    'requests',
-    'Click',
-    'pyfiglet'
-    ],
-    entry_points='''
-        [console_scripts]
-        yourscript=gecko.py:cli
-    ''',
-)
+with open('requirements.txt') as f: 
+	requirements = f.readlines() 
+
+long_description = 'Gecko Website Cloner/Copier'
+
+setup( 
+		name ='gecko', 
+		version ='1.0.0', 
+		author ='Mobolaji Abdulsalam', 
+		author_email ='ibraheemabdulsalam@gmail.com', 
+		url ='https://github.com/Vibhu-Agarwal/vibhu4gfg', 
+		description ='Gecko Website Cloner/Copier', 
+		long_description = long_description, 
+		long_description_content_type ="text/markdown", 
+		license ='MIT', 
+		packages = find_packages(), 
+		entry_points ={ 
+			'console_scripts': [ 
+				'gfg = vibhu4gfg.gfg:main'
+			] 
+		}, 
+		classifiers =( 
+			"Programming Language :: Python :: 3", 
+			"License :: OSI Approved :: MIT License", 
+			"Operating System :: OS Independent", 
+		), 
+		keywords ='geeksforgeeks gfg article python package vibhu4agarwal', 
+		install_requires = requirements, 
+		zip_safe = False
+) 

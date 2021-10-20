65139a42d0c1132c4bc4174754edaba522156682
Mobolaji Abdulsalam <ibraheemabdulsalam@gmail.com>
2021-10-20 11:48:32 -0700

    Update cloner.py

--- cloner.py
+++ cloner.py
@@ -1,13 +1,14 @@
+import sys
+import os 
+from datetime import datetime
+import random
 import asyncio
 import requests
-import urllib 
-import os           
+import urllib          
 import time
-import sys
-from pyfiglet import Figlet
+import pyfiglet
 import click
 from bs4 import BeautifulSoup as BSoup
-import random
 user_agent_list = [
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
@@ -198,3 +199,55 @@ class InternetConnectionError(Error):
 class ValueTooLargeError(Error):
     pass
 """     
+def gui():
+    import PySimpleGUI as psg
+    layout =[
+    [psg.Text('URL:')],
+    [psg.Input(key='url')],
+    [psg.Text('Save Location:')],
+    [psg.FolderBrowse("Click to choose save location", initial_folder="./",
+                      tooltip="Click to select folder", key='loc')],
+    [psg.Input(key='loc_str', readonly=True)],
+    [psg.Exit(), psg.Submit()],
+    ]
+
+    win = psg.Window('Gecko Cloner', layout)
+    while True:
+        clicked, entry = win.read()
+        print(entry)
+        win['loc_str'].update(value = str(entry['loc']))
+        if clicked in ["Exit"]:
+            break
+        elif clicked == 'Submit':
+            x = cloner.Clone(entry['url'])
+            return x.run()
+            print('Completed')
+    #win.close()
+    
+@click.group()
+@click.version_option("1.0.0")
+def cli():
+    font = random.choice(['dotmatrix','doom', 'coinstak', 'starwars', 'block'][:-2])
+    color = random.choice(['red','green', 'yellow','blue'])
+    f = pyfiglet.figlet_format('Gecko', font=font)
+    click.echo(click.style(f, fg=color, blink=True, bold=True))
+    print(f"Mobolaji Abdulsalam - {datetime.now().year}\n\n")
+    #print("1. Continue")
+    #print("2. GUI")
+    
+@cli.command(name='gecko')
+@click.option('--url', '-u', type=str, required=True, prompt="URL" , help='Enter a valid website address')
+@click.option("--dirs", type=str, prompt="Your name", help="Save location", default = "./")
+def comm(url, mission, dirs):
+    if not url:
+        raise Exception('bad')
+    x=cloner.Clone(url)
+    click.echo(x.run())
+
+@cli.command(name='gui')
+def wall():
+    return gui()
+#"""
+if __name__ == '__main__':
+    cli()
+#"""
